eba68ed458ab81ddbb5249ea578dd71baf49e0ad
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-13 12:01:07 -0500

    commit - SumaSerieService .

--- src/dao/SumaSerieDAO.java
+++ src/dao/SumaSerieDAO.java
@@ -21,4 +21,4 @@ public class SumaSerieDAO {
             e.printStackTrace();
         }
     }
-}   
+}
--- src/service/SumaSerieService.java
+++ src/service/SumaSerieService.java
@@ -1,4 +1,29 @@
 package service;
 
 public class SumaSerieService {
-}
+
+    public StringBuilder generarProcedimiento(int n) {
+        StringBuilder procedimiento = new StringBuilder();
+        int suma = 0;
+
+        for (int i = 1; i <= n; i += 4) {
+            suma += i;
+            procedimiento.append(i);
+
+            if (i + 4 <= n) {
+                procedimiento.append(" + ");
+            }
+        }
+
+        procedimiento.append(" = ").append(suma);
+        return procedimiento;
+    }
+
+    public int calcularSuma(int n) {
+        int suma = 0;
+        for (int i = 1; i <= n; i += 4) {
+            suma += i;
+        }
+        return suma;
+    }
+}   
